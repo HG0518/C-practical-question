45fc8a546f97ebb3001a23b8ceb3cadc1c19d3a6
Lee Heegeon <hptlord@gmail.com>
2023-09-13 10:34:35 +0900

    ch01 practical question

---
+++ ch01/ch01_2/1_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "컴퓨터공학과" << endl;
+	cout << "26세" << endl;
+	cout << "게임개발자" << endl;
+
+	return 0;
+}
---
+++ ch01/ch01_3/1_3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << 1 << "에서 " << 10 << "까지 더한 결과는 " << 55 << "입니다." << endl;
+	return 0;
+}
